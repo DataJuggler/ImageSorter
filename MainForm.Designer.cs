1f0255535c6a424b8ec266710e660389d4761ba1
DataJuggler <corby@datajuggler.com>
2022-11-30 12:54:04 -0600

    Checking in with DataJuggler.PixelDatabase updated.

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -220,7 +220,7 @@ namespace ImageSorter
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Image Sorter 1.1.0";
+            this.Text = "Image Sorter 1.1.1";
             this.ResumeLayout(false);
 
             }
